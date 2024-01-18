9eb86a82415f771480d7eea2491fe4d4fb3d78b0
Enrique Hervás Guerrero <enrique_hervas_94@hotmail.com>
2024-01-18 12:41:28 +0100

    Add the customer dimension to the gold zone

---
+++ gold/dim_customers.py
@@ -0,0 +1,35 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, when
+from credentials import sfOptionsSilver, sfOptionsGold
+
+# Initialize a Spark session with the necessary Snowflake JARs
+spark = SparkSession.builder.appName("dim_customers") \
+    .config("spark.jars", "../jars/snowflake-jdbc-3.14.4.jar,../jars/spark-snowflake_2.12-2.12.0-spark_3.3.jar") \
+    .getOrCreate()
+
+# Reads from the silver base
+dim_customers = spark.read.format("net.snowflake.spark.snowflake") \
+    .options(**sfOptionsSilver) \
+    .option("dbtable", "CSV.TRF_CUSTOMERS") \
+    .load()
+
+# Categorizing age
+dim_customers = dim_customers.withColumn('AGE_GROUP',
+                                         when(col('AGE') < 30, 'Young')
+                                         .when(col('AGE') < 60, 'Adult')
+                                         .otherwise('Senior'))
+
+# Reorder columns
+dim_customers = dim_customers.select('CUSTOMER_ID', 'CUSTOMER_NAME', 'EMAIL', 'PHONE', 'COUNTRY', 'CITY', 'ADDRESS',
+                                     'POSTAL_CODE', 'BIRTH_DATE', 'AGE', 'AGE_GROUP', 'GENDER', 'REGISTRATION_DATE',
+                                     'PURCHASE_HISTORY', 'LOYALTY_POINTS', 'LOAD_TIMESTAMP')
+dim_customers.show()
+# Write on the gold base
+dim_customers.write.format("net.snowflake.spark.snowflake") \
+    .options(**sfOptionsGold) \
+    .option("dbtable", "csv.dim_customers") \
+    .mode("append") \
+    .save()
+
+# Stop the Spark session
+spark.stop()
